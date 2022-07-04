ff59366e44fdee2b10829e41a6c3b1d26fa7b1a3
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-04 21:21:16 +0200

    SolarSystemSizes.java - konstruktorok létrehozva

--- SolarSystemSizes/src/solSys/SolarSystemSizes.java
+++ SolarSystemSizes/src/solSys/SolarSystemSizes.java
@@ -5,5 +5,18 @@ public class SolarSystemSizes {
 	private String name;
 	private String diameter;
 	private String distance;
+	
+	public SolarSystemSizes() {
+		
+	}
+	
+	public SolarSystemSizes(String name, String diameter, String distance) {
+		super();
+		this.name = name;
+		this.diameter = diameter;
+		this.distance = distance;
+	}
+	
+	
 
 }
