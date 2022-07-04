914996812e8b2e4d34c0387a22d2cc839302f5b2
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-04 21:26:34 +0200

    FileManager.java - bufferedreader deklarálva és definiálva

--- SolarSystemSizes/src/solSys/FileManager.java
+++ SolarSystemSizes/src/solSys/FileManager.java
@@ -1,5 +1,24 @@
 package solSys;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class FileManager {
+	
+	public List<SolarSystemSizes> solSysSize(String fileName) {
+		
+		List<SolarSystemSizes> solSysSizeList = new ArrayList<>();
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
