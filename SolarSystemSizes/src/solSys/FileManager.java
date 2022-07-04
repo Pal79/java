459f09234ebada5e04c4cf25c9e8d4e584198235
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-04 21:27:55 +0200

    FileManager.java - fejléc elemek vátozóba kiírva

--- SolarSystemSizes/src/solSys/FileManager.java
+++ SolarSystemSizes/src/solSys/FileManager.java
@@ -3,6 +3,7 @@ package solSys;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,12 @@ public class FileManager {
 		
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+			
+			String header = br.readLine();
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
