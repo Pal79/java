ffff5f44b18d5bea9eedbaf763ec252a00b5e0b2
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-05 07:11:38 +0200

    TaskManager.java - adatok alap kiírása

--- SolarSystemSizes/src/solSys/TaskManager.java
+++ SolarSystemSizes/src/solSys/TaskManager.java
@@ -1,5 +1,22 @@
 package solSys;
 
+import java.util.List;
+
 public class TaskManager {
+	
+	public void writingOutData(List<SolarSystemSizes> solSysList) {
+		
+		System.out.println("N = nap átmérője -> 1 392 700km");
+		
+		for(SolarSystemSizes ssList : solSysList) {
+			
+			System.out.println("Bolygó neve: " + ssList.getName());
+			System.out.println(ssList.getName() + " átmérője: " + ssList.getDiameter());
+			System.out.println(ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance());
+			System.out.println();
+			
+		}
+		
+	}
 
 }
