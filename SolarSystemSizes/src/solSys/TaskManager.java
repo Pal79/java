9f020676ffd257def378c2945a8287e510f9f9f1
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-05 15:06:49 +0200

    TaskManager.java - Plutó N paraméterének lecserélése a nap átmérőjének összegére

--- SolarSystemSizes/src/solSys/TaskManager.java
+++ SolarSystemSizes/src/solSys/TaskManager.java
@@ -7,7 +7,7 @@ public class TaskManager {
 	public void writingOutData(List<SolarSystemSizes> solSysList) {
 		
 		String sunDiameter = "1392700";
-		System.out.println("N = nap átmérője -> " + sunDiameter + "km");
+		System.out.println("N = nap átmérő -> 1 392 700km");
 		System.out.println();
 		
 		for(SolarSystemSizes ssList : solSysList) {
@@ -16,10 +16,18 @@ public class TaskManager {
 			String diameterCommaReplaceDot = diameterReplaceN.replace(",", ".");
 			String[] diameterSplit = diameterCommaReplaceDot.split("/");
 			
+			String distanceReplaceN = ssList.getDistance().replace("N", sunDiameter);
+			String distanceCommaReplaceDot = distanceReplaceN.replace(",", ".");
+			
 			System.out.println("Bolygó neve: " + ssList.getName());
 			System.out.print(ssList.getName() + " átmérője: " + ssList.getDiameter() + " = ");
 			System.out.println(Double.parseDouble(diameterSplit[0]) / Double.parseDouble(diameterSplit[1]) + "km");
-			System.out.println("A(z)" + ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance());
+			
+			if(ssList.getName().equals("Plutó")) {
+				String plutoReplaceN = ssList.getDistance().replace("N", sunDiameter);
+			}
+			System.out.print("A(z) " + ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance() + " = ");
+			
 			System.out.println();
 			
 		}
