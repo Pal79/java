44e86090fe0bca5c9457295a494f658c14595244
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-05 07:25:22 +0200

    TaskManager.java - feladatkezeő kiírásának kisebb csinosítása kiíráshoz

--- SolarSystemSizes/src/solSys/TaskManager.java
+++ SolarSystemSizes/src/solSys/TaskManager.java
@@ -7,12 +7,13 @@ public class TaskManager {
 	public void writingOutData(List<SolarSystemSizes> solSysList) {
 		
 		System.out.println("N = nap átmérője -> 1 392 700km");
+		System.out.println();
 		
 		for(SolarSystemSizes ssList : solSysList) {
 			
 			System.out.println("Bolygó neve: " + ssList.getName());
 			System.out.println(ssList.getName() + " átmérője: " + ssList.getDiameter());
-			System.out.println(ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance());
+			System.out.println("A(z)" + ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance());
 			System.out.println();
 			
 		}
