36dbd5c7d4983d29bccc8207c4fb6cbd06a3bd62
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-05 07:16:38 +0200

    Main.java - feladatkezelő objektumpéldány létrehozva

--- SolarSystemSizes/src/solSys/Main.java
+++ SolarSystemSizes/src/solSys/Main.java
@@ -1,9 +1,13 @@
 package solSys;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		FileManager fmObj = new FileManager();
+		List<SolarSystemSizes> solSysList = fmObj.solSysSize("solarSystemSizes.txt");
 
 	}
 
