c71a506560cf4997d9b85b993036762219aaa241
Pál Dániel <pal.daniel.79@gmail.com>
2022-07-05 12:34:51 +0200

    TaskManager.java - átmérő kiszámítása és kiírása

--- SolarSystemSizes/src/solSys/TaskManager.java
+++ SolarSystemSizes/src/solSys/TaskManager.java
@@ -6,13 +6,19 @@ public class TaskManager {
 	
 	public void writingOutData(List<SolarSystemSizes> solSysList) {
 		
-		System.out.println("N = nap átmérője -> 1 392 700km");
+		String sunDiameter = "1392700";
+		System.out.println("N = nap átmérője -> " + sunDiameter + "km");
 		System.out.println();
 		
 		for(SolarSystemSizes ssList : solSysList) {
 			
+			String diameterReplaceN = ssList.getDiameter().replace("N", sunDiameter);
+			String diameterCommaReplaceDot = diameterReplaceN.replace(",", ".");
+			String[] diameterSplit = diameterCommaReplaceDot.split("/");
+			
 			System.out.println("Bolygó neve: " + ssList.getName());
-			System.out.println(ssList.getName() + " átmérője: " + ssList.getDiameter());
+			System.out.print(ssList.getName() + " átmérője: " + ssList.getDiameter() + " = ");
+			System.out.println(Double.parseDouble(diameterSplit[0]) / Double.parseDouble(diameterSplit[1]) + "km");
 			System.out.println("A(z)" + ssList.getName() + " naphoz viszonyított távolsága: " + ssList.getDistance());
 			System.out.println();
 			
